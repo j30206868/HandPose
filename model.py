6d5ed2f52b8e97cda72013855332a2b5fa358227
Chen, Wen-Ju <jerry88114561@gmail.com>
2018-11-30 09:05:35 +0800

    Add tf_pose

--- model.py
+++ model.py
@@ -7,6 +7,7 @@ import cv2
 import matplotlib.pyplot as plt
 
 import scipy.stats as st
+import math
 import pdb
 
 def gauss_kernel(kernlen=21, nsig=3, channels=1):
@@ -72,7 +73,7 @@ def get_tfopenpose_hand_model(input_tensor):
     net_down_scale = 8
     return x, net_down_scale
 
-def remove_redundant(parts, heatmap):
+def remove_redundant(parts, heatmap, hand_amount=None):
     ''' remove redundant coordinates for each part
     
     Arguments:
@@ -97,7 +98,10 @@ def remove_redundant(parts, heatmap):
             structure is the same as 'parts' but coordinates are the deleted ones
     '''
     joint_type_amount = len(parts)
-    hand_amount = int(round(np.average([len(part_id_list) for part_id_list in parts])))
+
+    if hand_amount is None:
+        hand_amount = int(round(np.average([len(part_id_list) for part_id_list in parts])))
+    
     delted_part_list = [[] for i in range(joint_type_amount)]
     for part_id in range(len(parts)):
         joint_coords = parts[part_id]
@@ -145,14 +149,82 @@ def interchange_yx_to_xy_and_scale(parts, scale_factor):
             y, x = parts[part_id][cid]
             parts[part_id][cid] = (x*scale_factor, y*scale_factor)
 
+def interchange_yx_to_xy_and_normalize(parts, img_w, img_h):
+    for part_id  in range(len(parts)):
+        for cid in range(len(parts[part_id])):
+            y, x = parts[part_id][cid]
+            parts[part_id][cid] = (x/float(img_w), y/float(img_h))
+
 def draw_parts(image, parts, circle_color=(0,127,0), font_color=(0,255,0), font_scale=0.3):
     for part_id  in range(len(parts)):
         for coord in parts[part_id]:
             cv2.circle(image, coord, 10, circle_color)
             cv2.putText(image, str(part_id), coord, cv2.FONT_HERSHEY_COMPLEX, font_scale, font_color)
 
+def fastMax(a, b):
+    if (a > b): return a
+    else: return b
+
+def getHandROI(target_indice, body_parts, img_w, img_h):
+    assert(len(target_indice) == 3)
+    if set(target_indice) < set(body_parts):
+        ratioWristElbow = 0.33
+        shoulder, elbow, wrist = [body_parts[idx] for idx in target_indice]
+        distanceWristElbow = math.sqrt(pow((wrist.x - elbow.x)*img_w, 2) + pow((wrist.y - elbow.y)*img_h, 2))
+        distanceElbowShoulder = math.sqrt(pow((shoulder.x - elbow.x)*img_w, 2) + pow((shoulder.y - elbow.y)*img_h, 2))
+        box_w = 1.5 * fastMax(distanceWristElbow, 0.9 * distanceElbowShoulder)
+        x = (wrist.x + ratioWristElbow * (wrist.x - elbow.x)) * img_w
+        y = (wrist.y + ratioWristElbow * (wrist.y - elbow.y)) * img_h
+        x1 = x - box_w / 2.0
+        y1 = y - box_w / 2.0
+        x2 = x1 + box_w
+        y2 = y1 + box_w
+        x1 = max(x1, 0)
+        y1 = max(y1, 0)
+        x2 = min(x2, img_w-1)
+        y2 = min(y2, img_h-1)
+        return [int(round(x1)), int(round(y1)), int(round(x2))+1, int(round(y2))+1]
+        # return [int(round(x1)), int(round(y1)), int(round(x2-x1+1)), int(round(y2-y1+1))]
+    else:
+        return None
+
+class HandPart:
+    """
+    size: 'l' or 'r'
+    part_idx : part index(eg. 0 for nose)
+    x, y: coordinate of body part
+    score : confidence score
+    """
+    __slots__ = ('side', 'part_idx', 'x', 'y', 'score')
+
+    def __init__(self, side, part_idx, x, y, score):
+        self.side = side
+        self.part_idx = part_idx
+        self.x, self.y = x, y
+        self.score = score
+
+    def get_part_name(self):
+        return '%s Part %d' % (self.side, self.part_idx)
+
+    def __str__(self):
+        return 'HandPart:%s %d-(%.2f, %.2f) score=%.2f' % (self.side, self.part_idx, self.x, self.y, self.score)
+
+    def __repr__(self):
+        return self.__str__()
+
 class HandPose:
+    # l_shoulder_id = 5
+    # l_elbow_id = 6
+    # l_wrist_id = 7
+    # r_shoulder_id = 2
+    # r_elbow_id = 3
+    # r_wrist_id = 4
+    # left_indice = [l_shoulder_id, l_elbow_id, l_wrist_id]
+    # right_indice = [r_shoulder_id, r_elbow_id, r_wrist_id]
+
     def __init__(self, input_tensor):
+        self.coco_left_arm_body_indice = [5, 6, 7]
+        self.coco_right_arm_body_indice = [2, 3, 4]
         self.input_tensor = input_tensor
         self.sess = tf.Session(graph=tf.get_default_graph())
         ###
@@ -160,6 +232,8 @@ class HandPose:
         self.openpose_var_list = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
         ###
         self.is_pred_init = False
+        ###
+        self.joint_amount = self.openpose.shape[-1].value - 1
 
     def load_weights(self, tfckpt_path):
         saver = tf.train.Saver(var_list=self.openpose_var_list)
@@ -190,7 +264,26 @@ class HandPose:
         self.sess.run(tf.variables_initializer(uninit_vars))
         ###
         self.is_pred_init = True
-        
+
+    def getLeftHandRect(self, body_parts, img_w, img_h):
+        return getHandROI(self.coco_left_arm_body_indice, body_parts, img_w, img_h)
+
+    def getRightHandRect(self, body_parts, img_w, img_h):
+        return getHandROI(self.coco_right_arm_body_indice, body_parts, img_w, img_h)
+
+    def getLeftHandRoiImage(self, body_parts, image):
+        lbox = getHandROI(self.coco_left_arm_body_indice, body_parts, image.shape[1], image.shape[0])
+        if lbox is not None:
+            return image[lbox[1]:lbox[3],lbox[0]:lbox[2],:], lbox
+        return None, None
+
+    def getRightHandRoiImage(self, body_parts, image):
+        rbox = getHandROI(self.coco_right_arm_body_indice, body_parts, image.shape[1], image.shape[0])
+        if rbox is not None:
+            return image[rbox[1]:rbox[3],rbox[0]:rbox[2],:], rbox
+        return None, None
+
+    # for debug
     def predict(self, image_orig, scale_mul = 1.5, show_pred=False, show_removed=False):
         if not self.is_pred_init:
             self.init_predict_model()
@@ -199,6 +292,7 @@ class HandPose:
         scale = scale*scale_mul
         ###
         image = cv2.resize(image_orig, (0,0), fx=scale, fy=scale) 
+        # net_out.shape = input_tensor.shape / self.netscale
         net_out = self.sess.run(self.peaks_tensor, feed_dict={self.input_tensor: np.expand_dims( image /256 -0.5 ,0)})[0]
 
         peaks = np.argwhere(net_out >= 0.1)
@@ -223,3 +317,55 @@ class HandPose:
             plt.show()
 
         return parts
+
+    def fix_predict(self, image, handside, resize=True, hand_amount=None):
+        if not self.is_pred_init:
+            self.init_predict_model()
+    
+        if resize:
+            image = cv2.resize(image, (368, 368))
+        net_out = self.sess.run(self.peaks_tensor, feed_dict={self.input_tensor: np.expand_dims( image /256 -0.5 ,0)})[0]
+
+        peaks = np.argwhere(net_out >= 0.1)
+        parts = [[] for i in range(22)]
+        for y, x, part_id in peaks:
+            # y = peak[0], x = peak[1], part_id = peak[2]
+            parts[part_id].append(np.array([y, x]))
+
+        remove_redundant(parts, net_out, hand_amount=hand_amount)  
+        interchange_yx_to_xy_and_normalize(parts, image.shape[1]/self.netscale, image.shape[2]/self.netscale)  
+
+        hand = {}
+        for jid in range(self.joint_amount):
+            if len(parts[jid]) > 0:
+                x = parts[jid][0][0]
+                y = parts[jid][0][1]
+                hand[jid] = HandPart(handside, jid, x, y, 1)
+
+        return hand
+
+    def getHandsPart(self, image, body):
+        l_roi_img, lbox = self.getLeftHandRoiImage(body.body_parts, image)
+        r_roi_img, rbox = self.getRightHandRoiImage(body.body_parts, image)
+        
+        left_hand_parts = {}
+        right_hand_parts = {}
+        if l_roi_img is not None:
+            left_hand_parts = self.fix_predict(l_roi_img, 'l', hand_amount=1)
+
+        if r_roi_img is not None:
+            right_hand_parts = self.fix_predict(r_roi_img, 'r', hand_amount=1)
+        
+        lbox_w = lbox[2] - lbox[0]
+        lbox_h = lbox[3] - lbox[1]
+        for id in left_hand_parts:
+            left_hand_parts[id].x = lbox[0] + left_hand_parts[id].x*lbox_w
+            left_hand_parts[id].y = lbox[1] + left_hand_parts[id].y*lbox_h
+
+        rbox_w = rbox[2] - rbox[0]
+        rbox_h = rbox[3] - rbox[1]
+        for id in right_hand_parts:
+            right_hand_parts[id].x = rbox[0] + right_hand_parts[id].x*rbox_w
+            right_hand_parts[id].y = rbox[1] + right_hand_parts[id].y*rbox_h
+
+        return (left_hand_parts, right_hand_parts)
--- run.py
+++ run.py
@@ -2,12 +2,48 @@ import tensorflow as tf
 import numpy as np
 import cv2
 
+import tf_pose
+
 from model import *
 
-input_tensor = tf.placeholder(tf.float32, shape=(None, None, None,3), name='image')
+input_tensor = tf.placeholder(tf.float32, shape=(None, None, None, 3), name='image')
 handpose = HandPose(input_tensor)
 handpose.load_weights('checkpoints/pretrain')
 
-TEST_IMAGE_PATH = 'hand1.jpg'
-image_orig = cv2.imread(TEST_IMAGE_PATH)
-handpose.predict(image_orig, 1.5, True, True)
+# TEST_IMAGE_PATH = 'hand1.jpg'
+# image_orig = cv2.imread(TEST_IMAGE_PATH)
+# handpose.predict(image_orig, 1.5, True, True)
+
+
+
+import pdb
+import matplotlib.pyplot as plt
+import math
+
+
+
+TEST_IMAGE_PATH = 'hand2.jpg'
+image = cv2.imread(TEST_IMAGE_PATH)
+img_w = image.shape[1]
+img_h = image.shape[0]
+bodys = tf_pose.infer('hand2.jpg')
+for body in bodys:
+    # l_roi_image = handpose.getLeftHandRoiImage(body.body_parts, image)
+    # r_roi_image = handpose.getRightHandRoiImage(body.body_parts, image)
+    
+#     handpose.fix_predict(l_roi_image)
+    lhpts, rhpts = handpose.getHandsPart(image, body)
+    
+    for partid in lhpts:
+        part = lhpts[partid]
+        cv2.circle(image, (int(img_w*part.x), int(img_h*part.y)), 10, (255,0,0), 2)
+    for partid in rhpts:
+        part = rhpts[partid]
+        cv2.circle(image, (int(img_w*part.x), int(img_h*part.y)), 10, (255,0,0), 2)
+#     for partid in body.body_parts:
+#         part = body.body_parts[partid]
+#         cv2.circle(image, (int(img_w*part.x), int(img_h*part.y)), 10, (255,0,0), 2)
+
+plt.figure(figsize=(12,12))
+plt.imshow(image)
+plt.show()
